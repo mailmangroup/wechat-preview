4f12cc7b040b755052d58d35aa1f8619844563e4
Fergus Jordan <fergus.jordan@gmail.com>
2015-10-21 12:40:59 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,163 @@
 # wechat-preview
+[![wechat-preview version](https://img.shields.io/badge/wechat--preview-v1.0.0-brightgreen.svg)](https://github.com/mailmangroup/wechat-preview/) [![License](http://img.shields.io/badge/License-MIT-blue.svg)](http://opensource.org/licenses/MIT)
+
+## Introduction
+To support WeChat, we had to build a live preview of how the post would look that could be used within our app. This would allow us to simulate and build upon the already existing user experience that WeChat has internally.
+
+## Installation
+```
+$ bower install wechat-preview
+```
+
+## Including wechat-preview
+### RequireJS
+```javascript
+require.config({
+    paths: {
+        wechatPreview: 'path/to/bower_components/wechat-preview/dist/wechat-preview.min'
+    }
+});
+
+require( [ 'wechatPreview' ], function( wechatPreview ) {
+    ...
+});
+```
+### Basic Script Include
+```html
+<script src="./bower_components/wechat-preview/dist/wechat-preview.min.js"></script>
+```
+### Include the CSS File
+```html
+<link rel="stylesheet" href="./bower_components/wechat-preview/dist/wechat-preview.css">
+```
+
+## Usage
+```javascript
+var preview = new wechatPreview({ ... }, { ... });
+
+// returns preview.el
+// document.body.appendChild( preview.el );
+
+preview.generate({ ... });
+```
+
+## API
+
+### Build
+
+Type: `object`
+
+This will specify the size of the container.
+
+### Content
+
+Type: `object`
+
+This will specify the content of the container
+
+```javascript
+content: { ... }
+```
+
+## wechatPreview.build Options
+
+### build.container 
+
+Type: `string` or `object`
+
+This will specify the size of the container. This can be set by either a predefined set of screen sizes through a string, or specific pixel heights and widths through an object. i.e.
+
+```javascript
+var build = {
+    container: {
+        height: 736, // 736px (iPhone 6 Plus width) is the maximum accepted height
+        width: 414 // 414px (iPhone 6 Plus height) is the maximum accepted width
+    }
+}
+```
+
+Whitelisted screen size strings:
+
+- iPhone 4
+- iPhone 5
+- iPhone 6
+- iPhone 6 Plus
+
+## wechatPreview.content Options
+
+### articles
+
+Type: `object`
+Default: `null`
+
+This is an array of article objects that each contains post content.
+
+```javascript
+content: {
+    articles: [
+        { ... },
+        { ... }
+    ]
+}
+```
+
+### content.postTime
+Type: `timestamp`
+Default: Current time
+
+This will act as the date the post was published on both the entire post wrapper and each article where the date appears.
+
+```javascript
+content: {
+    postTime: 1444905243050,
+    articles: [
+        { ... },
+        { ... }
+    ]
+}
+```
+
+## content.article Options
+
+### content.article.shadow
+
+Type: `boolean`
+Default: `false`
+
+If set to true, this creates a DOM element in that position that acts as a placeholder, this can be used for creating a position to place items should you be dragging and dropping content into the container.
+
+```javascript
+content: {
+    articles: [
+        { ... }
+        { shadow: true },
+        { ... }
+    ]
+}
+```
+
+The above example will generate a DOM element in between the two articles.
+
+### content.article.title
+
+Type: `string`
+
+Default: `Article Title`
+
+The article title is the title of each individual article.
+
+### content.article.description
+
+Type: `string`
+
+Default: `Lorem ipsum dolor sit amet sit amor`
+
+This will be the description of each individual article, this will only be visible on specific classes of articles.
+
+### content.article.image
+
+Type: `string`
+
+Default: `null`
+
+This is the image URL of the articles featured image.
---
+++ wechat-preview.js
@@ -0,0 +1,376 @@
+/*
+ * WeChat Preview
+ * Author: Fergus Jordan
+ * Version: 1.0.0
+ *
+ * Preview of content in WeChat's iOS app
+ */
+(function ( root, factory ) {
+
+	// AMD
+	if ( typeof define === 'function' && define.amd ) define( [], factory );
+
+	// COMMONJS
+	else if ( typeof exports === 'object' ) module.exports = factory();
+
+	// BROWSER GLOBAL
+	else root.wechatPreview = factory();
+
+}(this, function () {
+	'use strict';
+
+	// DECLARE ACCEPTED CONTAINER SIZES
+	var sizes = {
+		iphone4: { 
+			height: '480px', 
+			width: '320px' 
+		},
+		iphone5: { 
+			height: '568px', 
+			width: '320px' 
+		},
+		iphone6: { 
+			height: '667px', 
+			width: '375px' 
+		},
+		iphone6plus: { 
+			height: '736px', 
+			width: '414px' 
+		}
+	};
+
+	// EXTEND JAVASCRIPT OBJECT
+	function extend ( defaults, options ) {
+	    
+	    var extended = {};	    
+	    var prop;
+	   
+	    for (prop in defaults) {        
+	        if (Object.prototype.hasOwnProperty.call(defaults, prop)) extended[prop] = defaults[prop]; 
+	    }
+	    
+	    for (prop in options) {
+	    	if (Object.prototype.hasOwnProperty.call(options, prop) ) extended[prop] = options[prop];
+	    }
+
+	    return extended;
+
+	};
+
+	// CONVERT STRING TO LOWERCASE AND STRIP SPACES TO ENSURE INPUT MATCHES ACCEPTED SIZES
+	function normalizeString ( string ) {
+
+		var normalized = string.toLowerCase().replace(/ /g,'');
+		return normalized;
+
+	};
+
+	// CREATE CLASS HELPER FUNCTION
+	// ===============================================================================
+	// HAS CLASS
+	function hasClass ( el, className ) {
+
+		if (el.classList) return el.classList.contains(className);
+		else return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+
+	}
+	// ADD CLASS
+	function addClass( el, className ) {
+
+		if (el.classList) el.classList.add(className);
+		else el.className += ' ' + className;	
+		
+	}
+	// REMOVECLASS
+	function removeClass( el, className ) {
+
+		if (el.classList) el.classList.remove(className);
+		else el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+
+	}
+
+	// TODO: REGEX TO WRAP ENGLISH IN TITLE IN SPANS FOR LETTER SPACING
+	// ===============================================================================
+
+	// DECLARE CREATE ELEMENT FUNCTION
+	// ===============================================================================
+	function createElement ( element, className, target ) {
+
+		element = document.createElement( element );
+		if ( className ) element.className = className;
+		if ( target ) target.appendChild( element );
+
+		return element;
+
+	}
+
+
+	// PREVIEW CLASS CONSTRUCTOR
+	// ===============================================================================
+	function wechatPreview ( options ) {
+		
+		// ENSURE THAT THE CLASS IS CALLED WITH THE `new` CONSTRUCTOR
+		if ( !( this instanceof wechatPreview ) ) {
+			throw new TypeError( 'wechatPreview constructor cannot be called as a function.' );
+		}
+
+		// NEW PREVIEW OBJECT
+		options = extend({
+			container: false
+		}, options);
+
+		// BUILD CONTAINER ELEMENT
+		// ===========================================================================
+		if ( options.container ) {
+
+			this.el = createElement( 'div', 'wcp' );
+			// CREATE MAIN DIV
+			this.main = createElement( 'div', 'wcp-main', this.el );
+			// CREATE EMPTY POST WRAPPER AND APPANED TO MAIN
+			this.postWrapper = createElement( 'div', 'wcp-post-wrapper wcp-empty', this.main );
+			// CREATE TIMESTAMP AND APPEND TO POST WRAPPER
+			this.timestamp = createElement( 'div', 'wcp-timestamp', this.postWrapper );
+
+		} else if ( !options.container ) {
+
+			this.el = createElement( 'div' );
+			this.postWrapper = createElement( 'div', false, this.el );
+
+		}
+
+		// CREATE CONTENT WRAPPER AND APPEND TO POST WRAPPER
+		this.contentWrapper = createElement( 'div', 'wcp-content-wrapper', this.postWrapper );
+
+		if ( !options.container ) 
+			addClass( this.contentWrapper, 'wcp-gallery' );
+
+
+		// IF CONTAINER IS AN OBJECT > OBJECT VALUES MUST BE HEIGHT AND WIDTH
+		// ===========================================================================
+		if ( typeof options.container === 'object' && options.container.width && options.container.height ) {
+
+			if ( typeof options.container.width === 'number' && typeof options.container.height === 'number' ) {
+
+				options.container.width = options.container.width + 'px';
+				options.container.minHeight = options.container.height + 'px';
+
+			}
+				
+			this.el.style.width = options.container.width;
+			this.el.style.minHeight = options.container.height;
+				
+		}
+
+		// IF CONTAINER IS AN STRING > SET HEIGHT AND WIDTH ACCORDING TO WHITELIST SIZES
+		// ===========================================================================
+		else if ( typeof options.container == 'string' ) {
+
+			for ( var prop in sizes ) {
+									
+				if ( sizes.hasOwnProperty( prop ) && prop == normalizeString( options.container ) ) {
+
+					this.el.style.width = sizes[ prop ].width;
+					this.el.style.minHeight = sizes[ prop ].height;
+
+				}
+							
+			}
+
+		} else if ( typeof options.container != 'boolean' ) throw new TypeError( 'Container object contains invalid values' );
+
+	};
+
+	// DECLARE ARTICLE CREATOR FUNCTION
+	// =========================================================================
+	wechatPreview.prototype.article = function ( articleContent, articleContainer ) {
+
+		var article = createElement( 'div', 'wcp-article', articleContainer );
+
+		// IF THE ARTICLE ISNT A SHADOW > CREATE ARTICLE CONTENT
+		if ( !articleContent.shadow ) {
+
+			// CREATE ARTICLE TITLE EL
+			var articleTitleEl = createElement( 'div', 'wcp-article-title', article ),
+				articleTitle = createElement( 'p', false, articleTitleEl ),
+				articleDateEl = createElement( 'span', 'wcp-article-date', article ),
+				articleImageEl = createElement( 'div', 'wcp-article-image', article ),
+				articleDescriptionEl = createElement( 'div', 'wcp-article-description', article ),
+				readAllEl = createElement( 'div', 'wcp-read-all', article ),
+				readAll = createElement( 'span', false, readAllEl );
+			
+			// SET INNER HTML IF POST TYPE ISNT EMPTY
+			if ( articleContent ) {
+				
+				// SET ARTICLE TITLE
+				articleTitle.innerHTML = articleContent.title;
+
+				// SET ARTICLE DATE
+				articleDateEl.innerHTML = this.articleDate;
+
+				// SET ARTICLE IMAGE
+				var articleImage = createElement( 'img', false, articleImageEl );
+					articleImage.setAttribute( 'src', articleContent.image );
+
+				// SET ARTICLE DESCRIPTION
+				articleDescriptionEl.innerHTML = articleContent.description;
+				
+				// SET ARTICLE READ ALL
+				readAll.innerHTML = '阅读原文';
+			
+			} else if ( !articleContent ) {
+
+				// CREATE THREE EMPTY ARTICLE LINES
+				for ( var i = 0; i < 3; i++ ) createElement( 'div', 'wcp-text-line', articleDescriptionEl );
+
+			}
+
+		} else {
+
+			addClass( article, 'wcp-shadow' );
+
+			this.shadowContent = createElement( 'div', 'wcp-shadow-content', article );
+
+		}
+
+		return article;
+
+	};
+
+	// GENERATE THE CONTENT OF THE PREVIEW
+	// ============================================================================
+	wechatPreview.prototype.generate = function ( content ) {
+
+		// SET CONTENT
+		// =========================================================================
+
+		// SET INITIAL DEFAULT VALUES
+		var defaults = {
+			postTime: new Date(),
+			articles: []
+		};
+
+		// IF CONTENT HAS ARTICLES SET > PUSH DEFAULT VALUES FOR THE LENGTH OF ARTICLES
+		content = extend( defaults, content );
+
+		if ( content.articles ) {
+				
+			for ( var i = 0; i < content.articles.length; i++ ) {
+							
+				if ( !this.previous ) {
+
+					content.articles[ i ] = extend({
+						title: '微博正文',
+						description: '摘要',
+						image: '../kawo/kawo_components/meta/stock-images/1.jpg',
+						shadow: false
+					}, content.articles[ i ]);
+
+				} else if ( this.previous ) {
+
+					content.articles[ i ] = extend( this.previous.articles[ i ], content.articles[ i ] );
+
+					content = extend( this.previous, content );
+
+				}
+			
+			}
+
+		}
+
+		// FORMAT THE DATE
+		// =========================================================================
+		if ( content.postTime ) {
+
+			// SET DATE VARIABLES
+			// ----------------------------------------------------------------------
+			var	weekDayName = [ '星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六' ],
+				monthName = [ '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月' ],
+				date = new Date( content.postTime ),
+				setDate = date.getDate(),
+				now = new Date(),
+				nowDate = now.getDate(),
+				day = date.getDay(),
+				nowDay = now.getDay(),
+				dayAbbr = setDate + '日',
+				dayDifference = nowDate - setDate,
+				weekDay = weekDayName[ day ],
+				month = date.getMonth(),
+				nowMonth = now.getMonth(),
+				monthAbbr = monthName[ month ],
+				year = date.getFullYear(),
+				hour = ( '0' + date.getHours() ).slice( -2 ),
+				minute = ( '0' + date.getMinutes() ).slice( -2 ),
+				clock = hour + ':' + minute;
+
+			// FORMAT DATE
+			// ----------------------------------------------------------------------
+			// THE DATE IS TODAY
+			if ( nowDate == setDate && month == nowMonth )
+				this.postDate = clock; 
+			
+			// THE DATE IS YESTERDAY
+			else if ( day == nowDay - 1 && dayDifference < 7 )
+				this.postDate = '昨天 ' + clock;
+
+			// WITHIN LAST 7 DAYS
+			else if ( dayDifference < 7 && dayDifference > 0 ) 
+				this.postDate = weekDay + ' ' + clock;
+			
+			// OUTSIDE OF LAST 7 DAYS	
+			else this.postDate = year + '年' + monthAbbr + dayAbbr + ' ' + clock; 
+
+			// SET CONTENT
+			// ----------------------------------------------------------------------
+			if ( !this.articleDate ) this.articleDate = monthAbbr + dayAbbr;
+			if ( this.timestamp ) this.timestamp.innerHTML = this.postDate;
+
+		}
+
+		// IF THERE ARE NO ARTICLES > CREATE THE EMPTY POST
+		// =========================================================================
+		if ( content.articles.length == 0 ) {
+
+			addClass( this.postWrapper, 'wcp-empty' );
+			this.emptyArticle = this.article( false, this.contentWrapper );
+			addClass( this.emptyArticle, 'wcp-single' );
+
+		}
+
+		// IF THERE IS THE CORRECT AMOUNT OF ARTICLES > CREATE THE ARTICLES
+		// =========================================================================
+		if ( content.articles.length > 0 ) {
+
+			// IF THE POST WRAPPER HAS PREVIOUSLY SET CLASSES > REMOVE THEM
+			removeClass( this.postWrapper, 'wcp-empty' );
+
+			if ( content.articles.length > 1 )
+				removeClass( this.contentWrapper, 'wcp-gallery' );
+
+			// LOOP THROUGH ARTICLES TO CREATE
+			for ( var i = 0; i < content.articles.length && i < 9; i++ ) {
+
+				// CREATE THE ARTICLES
+				var article = this.article( content.articles[ i ], this.contentWrapper );
+
+				// IF IT IS THE FIRST ARTICLE OF MULTIPLE ARTICLES > ADD THE TOP CLASSNAME
+				if ( i == 0 && content.articles.length > 1 ) 
+					addClass( article, 'wcp-top' );
+
+				// IF IT IS THE ONLY ARTICLE > ADD THE SINGLE CLASSNAME
+				else if ( i == 0 && content.articles.length == 1 && !hasClass( this.contentWrapper, 'wcp-gallery' ) ) 
+					addClass( article, 'wcp-single' );
+
+				// IF NOT THE FIRST ARTICLE > ADD THE NORMAL CLASSNAME
+				else addClass( article, 'wcp-normal' );
+
+			}
+
+		}
+
+		this.previous = content;
+
+	}
+
+	return wechatPreview;
+
+}));
